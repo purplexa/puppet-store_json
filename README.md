783bc49744f73b1a64ea33baf02d6fa73a46fa51
Ryan Whitehurst <ryan@ryanwhitehurst.com>
2015-10-30 14:08:36 -0700

    First version of Puppet 4 module

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-store_json puppet report processor
+# store_json report processor
+
+This modules provides an alternative to the default `store` report processor
+that writes out JSON reports instead of serialized Ruby YAML. Compatible with
+Puppet 4. For Puppet 3.4.3, use the `puppet-3.4.3` branch, or the latest
+`v0.1.x` branch.
+
---
+++ lib/puppet/reports/store_json.rb
@@ -0,0 +1,92 @@
+require 'puppet'
+require 'fileutils'
+require 'puppet/util'
+
+SEPARATOR = [Regexp.escape(File::SEPARATOR.to_s), Regexp.escape(File::ALT_SEPARATOR.to_s)].join
+
+Puppet::Reports.register_report(:store) do
+  desc "Store the yaml report on disk.  Each host sends its report as a YAML dump
+    and this just stores the file on disk, in the `reportdir` directory.
+
+    These files collect quickly -- one every half hour -- so it is a good idea
+    to perform some maintenance on them if you use this report (it's the only
+    default report)."
+
+  def process
+    validate_host(host)
+
+    dir = File.join(Puppet[:reportdir], host)
+
+    if ! Puppet::FileSystem.exist?(dir)
+      FileUtils.mkdir_p(dir)
+      FileUtils.chmod_R(0750, dir)
+    end
+
+    # Now store the report.
+    now = Time.now.gmtime
+    name = %w{year month day hour min}.collect do |method|
+      # Make sure we're at least two digits everywhere
+      "%02d" % now.send(method).to_s
+    end.join("") + ".json"
+
+    file = File.join(dir, name)
+
+    begin
+      Puppet::Util.replace_file(file, 0640) do |fh|
+        jsonified = self.to_data_hash
+        if jsonified['logs'].first.respond_to? :to_data_hash
+          jsonified['logs'] = jsonified['logs'].map do |log|
+            newlog = log.to_data_hash
+            if log.tags.respond_to? :to_data_hash
+              newlog['tags'] = log.tags.to_data_hash
+            end
+            newlog
+          end
+        end
+        if jsonified['metrics'].values.first.respond_to? :to_data_hash
+          jsonified['metrics'].each { |k,v| jsonified['metrics'][k] = v.to_data_hash }
+        end
+        if jsonified['resource_statuses'].values.first.respond_to? :to_data_hash
+          jsonified['resource_statuses'].each do |k,v|
+            jsonified['resource_statuses'][k] = v.to_data_hash
+            if v.events.first.respond_to? :to_data_hash
+              jsonified['resource_statuses'][k]['events'] = v.events.map { |x| x.to_data_hash }
+            end
+            if v.tags.respond_to? :to_data_hash
+              jsonified['resource_statuses'][k]['tags'] = v.tags.to_data_hash
+            end
+          end
+        end
+        fh.print(JSON.generate(jsonified))
+      end
+    rescue => detail
+       Puppet.log_exception(detail, "Could not write report for #{host} at #{file}: #{detail}")
+    end
+
+    # Only testing cares about the return value
+    file
+  end
+
+  # removes all reports for a given host?
+  def self.destroy(host)
+    validate_host(host)
+
+    dir = File.join(Puppet[:reportdir], host)
+
+    if Puppet::FileSystem.exist?(dir)
+      Dir.entries(dir).each do |file|
+        next if ['.','..'].include?(file)
+        file = File.join(dir, file)
+        Puppet::FileSystem.unlink(file) if File.file?(file)
+      end
+      Dir.rmdir(dir)
+    end
+  end
+
+  def validate_host(host)
+    if host =~ Regexp.union(/[#{SEPARATOR}]/, /\A\.\.?\Z/)
+      raise ArgumentError, "Invalid node name #{host.inspect}"
+    end
+  end
+  module_function :validate_host
+end
